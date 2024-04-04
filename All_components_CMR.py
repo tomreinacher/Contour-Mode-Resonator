d95efa0dc3038477a31c58c813c556ad312e2374
tomreinacher <158587159+tomreinacher@users.noreply.github.com>
2024-04-04 11:25:15 +0100

    Add files via upload
    Flat edge CMR with taper down to tether width. Tether width and angle are arguments of the method flat_cmr. Rotate about angle not working.

--- All_components_CMR.py
+++ All_components_CMR.py
@@ -17,7 +17,19 @@ resist_layer = (2,0)
 pad_width = 100
 pad_height = 50
 
-arm_width= 5 #width of arm between pad and taper
+arm_width= 15 #width of arm between pad and taper
+
+########################################
+#Etch window properties
+etch_window_gap = 5
+etch_buffer = 1 #distance between IDTs and etch window
+
+#####################################
+#Tether dimensions
+tether_width = 5 #width of tether polygon
+tether_length = etch_window_gap
+
+taper_length = 20
 
 #####################################
 #IDT properties
@@ -30,53 +42,101 @@ electrode_end_margin = 10  #distance between bus and electrode of opposite poten
 
 bus_width = 5 #width of metal electrode connecting idt fingers
 
-########################################
-#Etch window properties
-etch_window_gap = 5
-etch_buffer = 1 #distance between IDTs and etch window
+angle = 0 #angle of the entire CMR component - label 
 
-def flat_cmr(originx,originy,electrode_number,electrode_separation,electrode_width):
-    c = gf.Component("pad_and_bus")
+######################################################################
+#                      Flat-edge CMR Method                          #
+######################################################################   
+'''Instead of porting each individual IDT finger to the bus, a union is done between all metallized parts to create one solid metal layer component.
+ The etch windows are also defined using unions instead of ports. The bus/pad and route are done using ports and defined more correctly.'''
+
+def flat_cmr(originx,originy,electrode_number,electrode_separation,electrode_width,tether_width,angle):
+    #c = gf.Component("pad_and_bus")
     
     ################Add one bus, tether, taper + port###################
+    bus = gf.Component("bus")
+
     bus_length = electrode_number*(electrode_width+electrode_separation)-electrode_separation #length of metal electrode connecting idt fingers
 
-    bus = c.add_polygon(
+    p1 = bus.add_polygon(
         [(originx,originx,bus_width,bus_width),(originy,bus_length,bus_length,originy)],layer=metal_layer
     )
 
-    c.add_port(
-        name="bus_port",center=[0,bus_length/2],width=arm_width,orientation=180,layer=metal_layer #standard orientation of port is parallel to y axis
+    bus.add_port(
+        name="bus_port",center=[originx,bus_length/2],width=tether_width,orientation=180,layer=metal_layer #standard orientation of port is parallel to y axis
+    )
+
+    tether = gf.Component("tether")
+
+    p2 = tether.add_polygon(
+        [(originx,originx,originx+tether_length,originx+tether_length),(originy,originy+tether_width,originy+tether_width,originy)]
+    )
+    
+    tether.add_port(
+        name="tether_port1",center=[originx,tether_width/2],width=tether_width,orientation=180,layer=metal_layer #standard orientation of port is parallel to y axis
+    )
+    tether.add_port(
+        name="tether_port2",center=[originx+tether_length,tether_width/2],width=tether_width,orientation=0,layer=metal_layer #standard orientation of port is parallel to y axis
+    )
+    
+    taper =  gf.components.taper(
+        length = taper_length,
+        width1 = arm_width,
+        width2 = tether_width,
+        with_two_ports = True,
+        port_order_name = ("taper_port1","taper_port2"),
+        layer = metal_layer
+    )
+
+    connect_parts = gf.Component("connect_parts")
+    c1 = connect_parts << bus
+    c2 = connect_parts << tether
+    c3 = connect_parts << taper
+
+    c2.connect("tether_port2",c1.ports["bus_port"])
+    c3.connect("taper_port2",c2.ports["tether_port1"])
+
+    taper_port_x = originx-tether_length-taper_length
+    taper_port_y = bus_length/2
+
+    #must add component-level reference to uderlying subcomponent port
+    connect_parts.add_port(
+       name="taper_port",port=c3.ports["taper_port1"] 
     )
 
     ##############Add one pad + port #######################
+    pad = gf.Component("pad")
+    
     pad_originx = originx-3*pad_width/4
-    pad_originy = originy+2*bus_length 
+    pad_originy = originy+2*29.75 #fixed height based on longest bus length with 60 fingers
 
-    pad = c.add_polygon(
+    pad.add_polygon(
         [(pad_originx,pad_originx,pad_originx+pad_width,pad_originx+pad_width),(pad_originy,pad_originy+pad_height,pad_originy+pad_height,pad_originy)],layer=metal_layer
     )
-    c.add_port(
+    pad.add_port(
         name="pad_port",center=[pad_originx+arm_width/2,pad_originy],width=arm_width,orientation=270,layer=metal_layer
     )
 
     ##########Get route between bus and pad#################
+    pad_and_bus = gf.Component("pad_and_bus")
+
+    port1 = pad_and_bus << connect_parts
+    port2 = pad_and_bus << pad
 
     route = gf.routing.get_route(
-        c.ports["bus_port"], 
-        c.ports["pad_port"],
+        port1.ports["taper_port"], 
+        port2.ports["pad_port"],
         width = arm_width
     )
-    c.add(route.references)
+    pad_and_bus.add(route.references)
 
     ##########Mirror bus and pad about center of IDT#############
-    
     mirror_originx = originx+  bus_width + (electrode_length + electrode_end_margin)/2
     mirror_originy = originy + bus_length/2
 
-    pad_and_bus = gf.Component("pad_and_bus_mirrored")
-    bus_and_pad_1 = pad_and_bus << c
-    bus_and_pad_2 = pad_and_bus << c
+    pad_and_bus_complete = gf.Component("pad_and_bus_mirrored")
+    bus_and_pad_1 = pad_and_bus_complete << pad_and_bus
+    bus_and_pad_2 = pad_and_bus_complete << pad_and_bus
     bus_and_pad_2.mirror(p1=[mirror_originx,0],p2=[mirror_originx,mirror_originy])
 
     ##########Add IDT electrodes###########################
@@ -99,43 +159,15 @@ def flat_cmr(originx,originy,electrode_number,electrode_separation,electrode_wid
         electrodes.append(electrode_i)
     
     union_component = gf.Component("complete_component")
-    union_component << pad_and_bus
+    union_component << pad_and_bus_complete
     union_component << idt_array
     union_component = gf.geometry.union(union_component, by_layer=False, layer=metal_layer)
-
-    ############Create label for straight IDT###################
-    label = gf.Component("label")
-    text = f"Pair num = {electrode_number/2}\nPeriod = {2*(electrode_width+electrode_separation)}\nTether w = {arm_width+2*etch_buffer}"
-    
-    label_contents = label << gf.components.text(
-        text=text,
-       size=5,
-        position=[originx - 50, originy - 15],
-        justify='left',
-        layer=metal_layer
-    )
-
-    complete_component = gf.Component("straight idt and label")
-    complete_component << union_component
-    complete_component << label
-
-    ###########Create bounding box for HSQ#####################
-    #def get_bbox(complete_component):
-       # get_bbox = gf.Component("getting bbox")
-       # get_bbox << complete_component
-       # p1 = get_bbox.get_polygon_bbox()
-       # p2 = p1.buffer(10)
-       # return p2
-
-   # bbox_component = gf.Component("bbox_component")
-   # p2 = get_bbox(complete_component)
-   # bbox_component.add_polygon(p2, layer=resist_layer)
 
     ################Create etch windows#######################
     etch_window = gf.Component("etch_window") #define top etch window subcomponents
 
     etch_window_length = 2*bus_width + electrode_length + electrode_end_margin + 2*(etch_buffer + etch_window_gap) #horizontal window length
-    etch_window_height = bus_length/2 - arm_width/2 #vertical window length
+    etch_window_height = bus_length/2 - tether_width/2 #vertical window length
     
     top_x1 = originx-(etch_buffer + etch_window_gap)
     top_x2 = top_x1 + etch_window_length
@@ -144,12 +176,12 @@ def flat_cmr(originx,originy,electrode_number,electrode_separation,electrode_wid
 
     left_x1 = originx - etch_window_gap - etch_buffer
     left_x2 = left_x1 + etch_window_gap
-    left_y1 = bus_length/2 + arm_width/2 + etch_buffer
+    left_y1 = bus_length/2 + tether_width/2 + etch_buffer
     left_y2 = left_y1 + etch_window_height
 
     right_x1 = originx + 2*bus_width + electrode_length + electrode_end_margin + etch_buffer
     right_x2 = right_x1 + etch_window_gap
-    right_y1 = bus_length/2 + arm_width/2 + etch_buffer
+    right_y1 = bus_length/2 + tether_width/2 + etch_buffer
     right_y2 = right_y1 + etch_window_height
 
     #union acting weird and only taking two arguments so I have to make this dodgy fix and do two unions, sorry 
@@ -174,17 +206,40 @@ def flat_cmr(originx,originy,electrode_number,electrode_separation,electrode_wid
     bottom_window = etch_window_complete << etch_window_union2
     bottom_window.mirror(p1=[mirror_originx,mirror_originy],p2=[mirror_p1,mirror_originy])
 
-    #hsq_layer = gf.Component("hsq_layer")
-    #hsq_layer << gf.geometry.boolean(A=bbox_component, B=etch_window_complete, operation="not", precision=1e-6, layer=resist_layer) #not operation to remove windows from hsq layer
+    ###########Make component including etch windows and CMR and rotate if necessary##################
+    CMR_component = gf.Component("CMR_component")
+    CMR_component << union_component
+    CMR_component << etch_window_complete
+    CMR_component.rotate(45) 
+
+
+    ############Create label for flat-edge CMR###################
+    label = gf.Component("label")
+    text = f"Pair num = {electrode_number/2}\nPeriod = {2*(electrode_width+electrode_separation)}\nTether w = {tether_width+2*etch_buffer}"
     
+    label_contents = label << gf.components.text(
+        text=text,
+        size=5,
+        position=[originx - 3*pad_width/4, originy - 15],
+        justify='left',
+        layer=metal_layer
+    )
+
+    #########Define final flat-edge CMR component#############
     final_component = gf.Component("final_component")
-    final_component << etch_window_complete
-    final_component << complete_component
+    final_component << CMR_component
+    final_component << label
 
     return final_component
 
-component = gf.Component("flat_cmr")
-component << flat_cmr(originx,originy,electrode_number,electrode_separation,electrode_width)
 
-component.write_gds("teststructures.gds")
-component.show()
+all_components = gf.Component("flat_CMR")
+c1 = all_components << flat_cmr(originx,originy,20,electrode_separation,electrode_width,tether_width,angle)
+c2 = all_components << flat_cmr(originx,originy,40,electrode_separation,electrode_width,10,45)
+c3 = all_components << flat_cmr(originx,originy,60,electrode_separation,electrode_width,10,45)
+
+c2.move([300,0])
+c3.move([600,0])
+
+all_components.write_gds("all_components.gds")
+all_components.show()
